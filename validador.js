c84d548a14b88423e037b670cafb4b4b4e938c09
Johan David Mendez Hawasly <hawaslycode@gmail.com>
2026-03-19 17:00:48 -0500

    Add files via upload

---
+++ validador.js
@@ -0,0 +1,29 @@
+
+const usuarioCorrecto = "hawasly";
+const passCorrecto = "12345";
+const intentosMaximos = 3;
+let intentos = 0;
+
+function validarAcceso() {
+    let usIng = document.getElementById("userInput").value;
+    let passIng = document.getElementById("passInput").value;
+
+    if (intentos >= intentosMaximos) {
+        alert("Usuario bloqueado. No puedes intentar más.");
+        return;
+    }
+
+    if (usIng === usuarioCorrecto && passIng === passCorrecto) {
+        alert("Bienvenido al sistema!");
+        intentos = 0; 
+    } else {
+        intentos++;
+        let restantes = intentosMaximos - intentos;
+
+        if (restantes > 0) {
+            alert(`Datos incorrectos. Te quedan ${restantes} intentos.`);
+        } else {
+            alert("Usuario bloqueado. Ha superado el número de intentos.");
+        }
+    }
+}
